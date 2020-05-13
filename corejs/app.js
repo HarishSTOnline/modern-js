4deaf83c73040fbfc25b007136245e6d411c0834
HarishST <harishstprojects@gmail.com>
2020-05-13 23:43:40 +0530

    :tada: Initial Commit for Modern JS

---
+++ corejs/app.js
@@ -0,0 +1,30 @@
+// -------------- Log to console ---------------
+
+// Arrays
+console.log([1, 2, 3, 4]);
+
+// Objects
+console.log({a:1, b:2, 'c': 4, 1: 6, 'd':'testing'});
+
+// Console Table
+console.table({a:1, b:2, 'c': 4, 1: 6, 'd':'testing'});
+
+// Error Printing
+console.error('This is an example for Error');
+
+// Warning Printing
+console.warn('This is an Example For Warning');
+
+// Time Taken
+console.time('Timer');
+  console.log('Just Printing');
+  console.log('Just Printing');
+  console.log('Just Printing');
+  console.log('Just Printing');
+  console.log('Just Printing');
+  console.log('Just Printing');
+console.timeEnd('Timer');
+
+
+// Console Clear
+console.clear();
